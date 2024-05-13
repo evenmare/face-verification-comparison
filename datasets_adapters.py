010d3b80e4ccb13043aeaa07694dc5b6adf856ff
Kirill Vershinin <kir9ver@gmail.com>
2024-05-13 20:12:46 +0300

    deepface -> vggface. verifier refactoring

--- datasets_adapters.py
+++ datasets_adapters.py
@@ -1,3 +1,5 @@
+import abc
+import math
 import pathlib
 import shutil
 
@@ -8,70 +10,97 @@ from termcolor import colored
 import pandas as pd
 
 
-class KYCDatasetAdapter:
-    """ Universal KYC Datasets Adapter """
+class BaseDatasetAdapter(abc.ABC):
+    """ Base Datasets Adapter """
     dataset_name: str
 
     sources_path: pathlib.PosixPath
     destination_path: pathlib.PosixPath
-    accordance_file_path: pathlib.PosixPath
 
-    image_extension: str = '.jpg'
-    
     def __str__(self) -> str:
         return self.dataset_name
-    
-    def __repr__(self) -> str:
-        return f'<KYCDatasetAdapter: {str(self)}>'
 
     def __init__(
             self,
             sources_path: str,
-            accordande_file_path: str,
             destination_path: str,
             dataset_name: str | None = None,
     ):
         """
         :param sources_path: dataset images folder path ("{dataset_path}/files")
-        :param accordance_file_path: accordance file path (usually named as *_dataset.csv)
         :param destination_path: destination data absolute path (after mixing data)
         :param dataset_name: name of dataset (defaults to None, class property set to folder name)
         """
         self.sources_path = pathlib.Path(sources_path)
-        self.accordance_file_path = pathlib.Path(accordande_file_path)
         self.destination_path = pathlib.Path(destination_path)
 
         if not self.sources_path.is_dir():
             raise ValueError('sources_path: should be a directory path')
+
+        self.dataset_name = dataset_name or self.sources_path.name
+
+    @abc.abstractmethod
+    def prepare_data(self) -> None:
+        raise NotImplementedError('prepare_data')
+
+    @property
+    @abc.abstractmethod
+    def total(self) -> int:
+        raise NotImplementedError('total')
+
+
+class KYCDatasetAdapter(BaseDatasetAdapter):
+    """ Universal KYC Datasets Adapter """
+    accordance_file_path: pathlib.PosixPath
+
+    image_extension: str = '.jpg'
+
+    def __repr__(self) -> str:
+        return f'<KYCDatasetAdapter: {str(self)}>'
+
+    def __init__(
+            self,
+            *args,
+            accordande_file_path: str,
+            **kwargs,
+    ):
+        """
+        :param accordance_file_path: accordance file path (usually named as *_dataset.csv)
+        """
+        super().__init__(*args, **kwargs)
+
+        self.accordance_file_path = pathlib.Path(accordande_file_path)
 
         if not self.accordance_file_path.is_file():
             raise ValueError('accordance_file_path: should be a file path')
         if self.accordance_file_path.suffix.lower() != '.csv':
             print(self.accordance_file_path.suffix)
             raise ValueError('accordance_file_path: should be a .csv file')
-        
-        self.root_images_path = self.destination_path / 'roots'
-        
-        self.dataset_name = dataset_name or self.sources_path.name
-    
+
+        self.id_images_path = self.destination_path / 'roots'
+
     @cached_property
     def _accordance_raw(self) -> pd.DataFrame:
         return pd.read_csv(self.accordance_file_path, sep=',')
-    
+
+    @property
+    def total(self) -> int:
+        return math.prod(self._accordance_raw.loc[:, 'id_2':].shape)
+
     @cached_property
-    def _root_images_mapping_raw(self) -> dict[int, str]:
+    def _id_images_mapping_raw(self) -> dict[int, str]:
         return dict(self._accordance_raw['id_1'])
-    
+
     def _get_target_path(self, filepath: pathlib.Path, image_prefix: str | None = None) -> pathlib.Path:
         image_prefix = image_prefix or ''
         target_filename = f'{image_prefix}{filepath.name}'
-    
+
         new_filepath = self.destination_path / target_filename
-        for root_image_path in self._root_images_mapping_raw.values():
-            if f'{filepath.parts[-2]}/{filepath.parts[-1]}' == root_image_path.strip('/'):
-                new_filepath = self.root_images_path / target_filename
+        for id_image_path in self._id_images_mapping_raw.values():
+            if f'{filepath.parts[-2]}/{filepath.parts[-1]}' == id_image_path.strip('/'):
+                new_filepath = self.id_images_path / target_filename
                 break
-        
+
         return new_filepath
 
     def prepare_data(self) -> None:
@@ -84,7 +113,7 @@ class KYCDatasetAdapter:
 
         # create destination folder
         self.destination_path.mkdir(exist_ok=False, parents=True)
-        self.root_images_path.mkdir(exist_ok=False)
+        self.id_images_path.mkdir(exist_ok=False)
         print(colored('created', 'green'))
 
         # copy files
@@ -95,10 +124,10 @@ class KYCDatasetAdapter:
 
                 print(f'    Processing combination {folder.name}...'.ljust(50), end='')
                 for file in folder.iterdir():
-                    if file.suffix.lower() == self.image_extension:                        
+                    if file.suffix.lower() == self.image_extension:
                         shutil.copy(file, self._get_target_path(file, image_prefix))
                 print(colored('completed', 'green'))
-    
+
     @cached_property
     def accordance(self) -> pd.DataFrame:
         """
@@ -106,31 +135,31 @@ class KYCDatasetAdapter:
         :returns: dataframe
         """
         df = self._accordance_raw.copy()
-        
+
         for column_name in df.columns.array[df.columns.get_loc('id_1'):]:
             for y in range(len(df)):
                 df.loc[y, column_name] = '_'.join(df[column_name][y].strip('/').split('/'))
-        
+
         return df
-    
+
     @cached_property
-    def root_to_images_mapping(self) -> dict[int, str]:
+    def id_to_images_mapping(self) -> dict[int, str]:
         """
-        Root images to images set mapping
-        :returns: {root image filename: {image path 1, image path 2, ...}}
+        ID images to images set mapping
+        :returns: {id image filename: {image path 1, image path 2, ...}}
         """
         return {
-            root_image_path: set(self.accordance.loc[i, 'id_2':].tolist())
-            for i, root_image_path in enumerate(self.accordance['id_1'])
+            id_image_path: set(self.accordance.loc[i, 'id_2':].tolist())
+            for i, id_image_path in enumerate(self.accordance['id_1'])
         }
-    
-    def root_images_paths_iterator(self) -> Iterator[str]:
+
+    def id_images_paths_iterator(self) -> Iterator[str]:
         """
-        Root images paths iterator
-        :yield: path of root image
+        ID images paths iterator
+        :yield: path of id image
         """
-        for root_image_file in self.root_images_path.iterdir():
-            yield root_image_file.absolute()
+        for id_image_file in self.id_images_path.iterdir():
+            yield id_image_file.absolute()
 
     def images_paths_iterator(self) -> Iterator[str]:
         """
--- verifiers.py
+++ verifiers.py
@@ -1,66 +1,117 @@
+from typing import TypeVar
+
+import pandas as pd
 from deepface import DeepFace
+from tqdm import tqdm
 
 from datasets_adapters import KYCDatasetAdapter
 
 
-class ModelVerifier:
-    """ Class that implements functionality to verify model """
-    model_name: str
+DatasetAdapterVar = TypeVar('DatasetAdapterVar', bound=KYCDatasetAdapter)
+
+
+class ModelMetricSet:
+    """ Class that implements functionality to count model metrics """
+    identifier: str
 
     tp: int
     tn: int
     fp: int
     fn: int
 
-    def __init__(self, model_name: str) -> None:
-        self.model_name = model_name
-    
-    def _clear(self):
+    accuracy: float
+    recall: float
+    fpr: float
+    precision: float
+    f_score: float
+
+    def __str__(self) -> str:
+        return f'Metric {self.identifier}'
+
+    def __init__(self, identifier: str) -> None:
+        """
+        :param identifier: metric set identifier
+        """
+        self.identifier = identifier
+
         self.tp = 0
+        self.tn = 0
         self.fp = 0
         self.fn = 0
-        self.tn = 0
-    
-    def _classify(self, _result: bool, _expected: bool):
-        if _result:
-            if _expected:
-                self.tp += 1
-            else:
-                self.fp += 1
-        else:
-            if _expected:
-                self.fn += 1
-            else:
-                self.tn += 1
 
-    def execute(
-            self,
-            dataset_adapter: KYCDatasetAdapter,
-    ):
-        self._clear()
+    def calculate(self) -> None:
+        """ Metrics set calculation """
+        self.accuracy = (self.tp + self.tn) / (self.tp + self.tn + self.fp + self.fn)
+        self.recall = self.tp / (self.tp + self.fn)
+        self.fpr = self.fp / (self.fp + self.tn)
+        self.precision = self.tp / (self.tp + self.fp)
+        self.f_score = 2 * (self.tp / (2 * self.tp + self.fn + self.fp))
 
-        total = 0
+    def to_dataframe(self) -> pd.DataFrame:
+        """ Returns metrics in dataset format """
+        details = dict(vars(self))
+        details.pop('identifier')
+        return pd.DataFrame({self.identifier: details})
+
+class ModelVerifier:
+    """ Class that implements functionality to verify model """
+    model_name: str
+    metrics_sets: list[ModelMetricSet]
+
+    def __init__(self, model_name: str) -> None:
+        """
+        :param model_name: name of model
+        """
+        self.metrics_sets = []
+        self.model_name = model_name
+
+    def execute(self, dataset_adapter: DatasetAdapterVar) -> pd.DataFrame:
+        """
+        Verification execution
+        :param dataset_adapter: adapter used to iterate by dataset
+        :return: metrics in dataframe format
+        """
+        execution_metric = ModelMetricSet(f'{self.model_name}:{str(dataset_adapter)}')
+
         print(f'Verification {self.model_name} on {str(dataset_adapter)}', end='\n\n')
 
-        for verification_image_path in dataset_adapter.images_paths_iterator():
-            print(f'Image path: {verification_image_path} ...'.ljust(50))
-            for root_image_path in dataset_adapter.root_images_paths_iterator():
-                print(f'Root image path: {root_image_path} ...'.ljust(50))
-                result = DeepFace.verify(
-                    root_image_path,
-                    verification_image_path,
-                    model_name=self.model_name,
-                )['verified']
-                expected = (verification_image_path.name in 
-                            dataset_adapter.root_to_images_mapping[root_image_path.name])
-                
-                print(verification_image_path)
-                print('    Result:'.ljust(50), result)
-                print('    Expected:'.ljust(50), expected)
+        with tqdm(total=dataset_adapter.total) as pbar:
+            for verification_image_path in dataset_adapter.images_paths_iterator():
+                for id_image_path in dataset_adapter.id_images_paths_iterator():
+                    try:
+                        result = DeepFace.verify(
+                            id_image_path,
+                            verification_image_path,
+                            model_name=self.model_name,
+                        )['verified']
+                    except ValueError:
+                        continue
 
-                self._classify(result, expected)
+                    expected = (verification_image_path.name in
+                                dataset_adapter.id_to_images_mapping[id_image_path.name])
 
-                total += 1
-        
-        self.counted_accuracy = (self.tp + self.tn) / (self.tp)
-        print(f'\nTOTAL ACCURACY: {(self.tp + self.tn) / total}')
+                    _classify_run(execution_metric, result, expected)
+
+                pbar.update(1)
+
+        execution_metric.calculate()
+        self.metrics_sets.append(execution_metric)
+
+        return execution_metric.to_dataframe()
+
+
+def _classify_run(
+        metric: ModelMetricSet,
+        _result: bool,
+        _expected: bool
+) -> None:
+    if _result:
+        if _expected:
+            metric.tp += 1
+        else:
+            metric.fp += 1
+    else:
+        if _expected:
+            metric.fn += 1
+        else:
+            metric.tn += 1
